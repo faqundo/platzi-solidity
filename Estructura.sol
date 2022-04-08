3c682d085c17cb43bee472a0956ff4db77520503
faqundo <facundo.marcial.ferro@gmail.com>
2022-04-08 16:06:09 -0300

    structs y arrays

--- Estructura.sol
+++ Estructura.sol
@@ -1,8 +1,15 @@
 //SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >0.7.0 < 0.9.0;
-contract Estructura {
-    constructor(){
+pragma solidity >0.7.0 <0.9.0;
 
+contract Estructura {
+    int256 cantidad;
+    uint256 cantidadSinSigno;
+    address direccion;
+    bool firmado;
+
+    constructor(bool estaFirmado) {
+        direccion = msg.sender;
+        firmado = estaFirmado;
     }
 }
